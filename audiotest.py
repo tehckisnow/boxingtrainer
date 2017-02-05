878925532a0bd26e03c8ad480125744ed2c06d37
tehckisnow <chetwonsik@gmail.com>
2017-02-05 01:29:34 -0500

    Audio working!
    Finally got audio working

---
+++ audiotest.py
@@ -0,0 +1,14 @@
+import winsound, time, random
+winsound.PlaySound("recording.wav", winsound.SND_FILENAME)
+time.sleep(5)
+winsound.PlaySound("beet.wav", winsound.SND_FILENAME)
+
+## import os
+## os.system("start C:/Recording.m4a")
+## os.system("start C:/Recording.m4a")
+
+techniques = [[jab, cross, hook], [catch, parry, shoulder roll]]
+for
+    random.randint
+clip =
+winsound.PlaySound(clip, winsound.SND_FILENAME)
