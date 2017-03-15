e1175a2d6395b686f7977f6bc078af9229d423cb
tehckisnow <chetwonsik@gmail.com>
2017-03-15 18:15:49 -0500

    Learning to use git
    I keep updating the master branch with the linux code.  fix'd, hopefully

--- boxingtrainer.py
+++ boxingtrainer.py
@@ -1,11 +1,12 @@
-import time, random, subprocess
+import winsound, time, random
 
-skill = 0       #time between strikes (.5 beginner, .01 advanced, 0 max)
-mintime = 1     #minimum time between combos(rcmnd 1)
-maxtime = 2     #maximum ause between combos (rcmnd 1-3)
-mincmbolngth = 2 #minimum combo length (rcmnd 1-2)
-cmbolngth = 4   #maximum number of techniques in a combo (rcmd 1-4)
-rndlngth = 1    #length of each round in minutes
+#numberofcombos = 25     #total number of combos to perform
+skill = 0             #time between strikes (.5 beginner, .01 advanced, 0 max)
+mintime = 1            #minimum time between combos(rcmnd 1)
+maxtime = 2  #maximum ause between combos (rcmnd 1-3)
+mincmbolngth = 2       #minimum combo length (rcmnd 1-2)
+cmbolngth = 4  #maximum number of techniques in a combo (rcmd 1-4)
+rndlngth = 1  #length of each round in minutes
 
 techniques = ["jab.wav", "jab.wav", "jab.wav",  "jab.wav", "cross.wav",
 "cross.wav", "cross.wav","leadhook.wav", "leaduppercut.wav", "leadelbow.wav",
@@ -13,19 +14,19 @@ techniques = ["jab.wav", "jab.wav", "jab.wav",  "jab.wav", "cross.wav",
 allhits = len(techniques)-1
 
 def round():
-    subprocess.call("aplay -q startbell.wav", shell=True)
+    winsound.PlaySound("startbell.wav", winsound.SND_FILENAME)
     time_end = time.time() + 60 * rndlngth
     while time.time() < time_end:
         combolength = random.randint(mincmbolngth, cmbolngth)
         for bam in range(combolength):
             num = random.randint(0, allhits)
             print(techniques[num])
-            subprocess.call("aplay -q " + techniques[num], shell=True)
+            winsound.PlaySound(techniques[num], winsound.SND_FILENAME)
             time.sleep(skill)
         print("    ")
         tbtc= random.randint(mintime, maxtime)  #time btween combos
         time.sleep(tbtc)
-    subprocess.call("aplay -q startbell.wav", shell=True)
+    winsound.PlaySound("startbell.wav", winsound.SND_FILENAME)
     start()
 
 def start():
